71d4a3ceae7b1d17c25d99fb3b5e61d2cf2e8fce
Nancy Kumari <nancykumari620@gamil.com>
2024-06-17 20:17:16 +0530

    Footer Done

---
+++ client/src/customer/components/Footer/Footer.jsx
@@ -0,0 +1,88 @@
+import { Button, Typography ,Grid} from "@mui/material";
+
+const Footer=()=>{
+    return (
+        <div>
+            <Grid className="bg-black text-white text-center mt-20"
+            container sx={{bgcolor:"black",color:"white",py:3}}>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className="pb-5" variant="h6">Company</Typography>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>about</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>press</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Blog</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Jobs</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Investor</Button>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className="pb-5" variant="h6">Company</Typography>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>about</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>press</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Blog</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Jobs</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Investor</Button>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className="pb-5" variant="h6">Company</Typography>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>about</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>press</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Blog</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Jobs</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Investor</Button>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className="pb-5" variant="h6">Company</Typography>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>about</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>press</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Blog</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Jobs</Button>
+                    </div>
+                    <div>
+                    <Button className="pb-5" variant="h6" gutterBottom>Investor</Button>
+                    </div>
+                </Grid>
+                  <Grid xs={12}>
+                    <Typography>hhjnjkmk</Typography>
+                    <Typography>jhbjnkjn</Typography>
+                  </Grid>
+            </Grid>
+        </div>
+    )
+}
+export default Footer;
