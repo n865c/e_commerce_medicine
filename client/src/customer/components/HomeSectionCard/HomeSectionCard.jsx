d86910efb4ec9d548c812c429c219cd9712bfe80
Nancy Kumari <nancykumari620@gamil.com>
2024-07-12 21:52:49 +0530

    homeSectionCard changes did

--- client/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ client/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const HomeSectionCard=({product})=>{
+    const navigate=useNavigate();
+    const handleClick=()=>{
+        navigate('/product/1')
+    }
     return(
-        <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 h-80 border">
+        <div onClick={handleClick} className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 h-80 border">
              <div className="h-[13rem] w-[10rem]">
               <img className="object-cover object-top w-full h-48" src={product?.image} alt=""/>
              </div>
